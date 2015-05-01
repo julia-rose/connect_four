d1704c787245866e6d8df0101f400784ce01a9be
Julia Rose <julmrose@gmail.com>
2015-05-01 13:50:32 -0400

    red, the blood of angry men/black, the dark of ages past

--- mad_scripts.js
+++ mad_scripts.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
 
-$(".piece").each(function(index, value) { 
-    $(this).click(function(){
-    	console.log("Piece " + index + " was clicked.");
-    	$(this).css("background-color", "red");
-  	});
-  });
+
+	function startGame() {
+		$(".piece").css("background-color", "white");
+
+		var turn = 1;
+
+		$(".piece").each(function(index, value) { 
+		  $(this).click(function(){
+		    	// console.log("Piece " + index + " was clicked.");
+		    if(turn % 2 === 0){
+		    	$(this).css("background-color", "red");
+		    } else{
+		    	$(this).css("background-color", "black");
+		    };
+		    turn++
+		  });
+		});
+
+	};
+
+	startGame();
 
 });
+
+
 
